56f5344fe53114ec695c796becb7fb8ea897b299
Alfonso Lara <orozcoenrique159@gmail.com>
2025-11-08 17:00:20 -0500

    se creó el menu

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       mostrarMenu();
+    }
+
+    static void mostrarMenu(){
+        var opciones = """
+                --- Sistema de Registro de Estudiantes ---
+
+                1. Registrar datos de un estudiante
+                2. Mostrar datos del estudiante actual
+                3. Calcular promedio de notas
+                4. Mostrar resumen completo del estudiante
+                5. Limpiar datos del estudiante actual
+                0. Salir
+                """;
+        System.out.println(opciones);
+        System.out.print("Seleccione una opción: ");
     }
 }
